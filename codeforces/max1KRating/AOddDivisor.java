d31f26aa336c42a8504e5ec8b7d3f21ba855d6aa
Niket <krnike96@gmail.com>
2026-07-27 22:05:46 +0530

    feat: codeforces - 1033B, 1370A & 1475A

---
+++ codeforces/max1KRating/AOddDivisor.java
@@ -0,0 +1,58 @@
+import java.util.*;
+import java.io.*;
+
+public class AOddDivisor {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException { return Integer.parseInt(next()); }
+    static long nextLong() throws IOException { return Long.parseLong(next()); }
+    static double nextDouble() throws IOException { return Double.parseDouble(next()); }
+    static char nextChar() throws IOException { return next().charAt(0); }
+
+    static String nextLine() throws IOException {
+        if (st != null && st.hasMoreTokens()) {
+            StringBuilder rest = new StringBuilder(st.nextToken());
+            while (st.hasMoreTokens()) rest.append(' ').append(st.nextToken());
+            return rest.toString();
+        }
+        return br.readLine();
+    }
+
+    static int[] nextIntArray(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = nextInt();
+        return arr;
+    }
+
+    static long[] nextLongArray(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) arr[i] = nextLong();
+        return arr;
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int t = nextInt();
+        while (t-- > 0) 
+            solve();
+        
+        System.out.print(sb);
+        System.out.flush();
+    }
+
+    static void solve() throws IOException {
+        long n = nextLong();
+
+        sb.append((n & (n - 1)) != 0 ? "YES\n" : "NO\n");
+    }
+}
