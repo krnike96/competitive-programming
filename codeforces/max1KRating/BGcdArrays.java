74aa2577c6d72edcd7adf758fc4f8d6cff72bbfd
Niket <krnike96@gmail.com>
2026-07-29 23:57:07 +0530

    feat: codeforces - 1629B

---
+++ codeforces/max1KRating/BGcdArrays.java
@@ -0,0 +1,76 @@
+import java.util.*;
+import java.io.*;
+
+public class BGcdArrays {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException { return Integer.parseInt(next()); }
+    static long nextLong() throws IOException { return Long.parseLong(next()); }
+    static double nextDouble() throws IOException { return Double.parseDouble(next()); }
+    static char nextChar() throws IOException { return next().charAt(0); }
+
+    static String nextLine() throws IOException {
+        if (st != null && st.hasMoreTokens()) {
+            StringBuilder rest = new StringBuilder(st.nextToken());
+            while (st.hasMoreTokens()) rest.append(' ').append(st.nextToken());
+            return rest.toString();
+        }
+        return br.readLine();
+    }
+
+    static int[] nextIntArray(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = nextInt();
+        return arr;
+    }
+
+    static long[] nextLongArray(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) arr[i] = nextLong();
+        return arr;
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int t = nextInt();
+        while (t-- > 0) 
+            solve();
+        
+        System.out.print(sb);
+        System.out.flush();
+    }
+
+    static void solve() throws IOException {
+        int l = nextInt();
+        int r = nextInt();
+        int k = nextInt();
+
+        if(l == r){
+            if(l == 1)
+                sb.append("NO\n");
+            else
+                sb.append("YES\n");
+            return;
+        }
+
+        int odds = (r - l + 1) / 2;
+
+        if(l % 2 != 0 && r % 2 != 0)
+            odds++;
+        
+        if(odds <= k)
+            sb.append("YES\n");
+        else
+            sb.append("NO\n");    
+    }
+}
