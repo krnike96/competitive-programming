9a57dfe3f62896509419d75888c3c726f25b8783
Niket <krnike96@gmail.com>
2026-08-01 18:36:30 +0530

    atcoder - beginner_contest_469 - trainCar, isolatedSeats, & canTrip

---
+++ atcoder/Cantrip.java
@@ -0,0 +1,78 @@
+import java.util.*;
+import java.io.*;
+
+public class Cantrip {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException { return Integer.parseInt(next()); }
+    static long nextLong() throws IOException { return Long.parseLong(next()); }
+    static double nextDouble() throws IOException { return Double.parseDouble(next()); }
+    static char nextChar() throws IOException { return next().charAt(0); }
+
+    static String nextLine() throws IOException {
+        if (st != null && st.hasMoreTokens()) {
+            StringBuilder rest = new StringBuilder(st.nextToken());
+            while (st.hasMoreTokens()) rest.append(' ').append(st.nextToken());
+            return rest.toString();
+        }
+        return br.readLine();
+    }
+
+    static int[] nextIntArray(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = nextInt();
+        return arr;
+    }
+
+    static long[] nextLongArray(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) arr[i] = nextLong();
+        return arr;
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        // int t = nextInt();
+        // while (t-- > 0) 
+            solve();
+        
+        System.out.print(sb);
+        System.out.flush();
+    }
+
+    static void solve() throws IOException {
+        int n = nextInt();
+        char[] s = next().toCharArray();
+
+        int[] firstPos = new int[n + 1];
+        Arrays.fill(firstPos, -1);
+
+        int currXFreq = 0;
+        for (int m = 1; m <= n; m++) {
+            if (s[m - 1] == 'x') {
+                currXFreq++;
+                if (firstPos[currXFreq] == -1) {
+                    firstPos[currXFreq] = m;
+                }
+            }
+        }
+
+        for (int k = 1; k <= n; k++) {
+            if (k <= currXFreq && firstPos[k] != -1) {
+                sb.append(firstPos[k]).append("\n");
+            } else {
+                sb.append(n).append("\n");
+            }
+        }
+    }
+}
