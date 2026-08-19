03604783660141d6613da4e1305df32f87de4090
krnike96 <krnike96@gmail.com>
2026-08-19 21:08:32 +0530

    feat: codeforces - 1606A

---
+++ codeforces/max1KRating/AAbBalance.java
@@ -0,0 +1,64 @@
+import java.util.*;
+import java.io.*;
+
+public class AAbBalance {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static String next() throws IOException {
+        while (st == null || !st.hasMoreTokens()) {
+            st = new StringTokenizer(br.readLine());
+        }
+        return st.nextToken();
+    }
+
+    static int nextInt() throws IOException { return Integer.parseInt(next()); }
+    static long nextLong() throws IOException { return Long.parseLong(next()); }
+    static double nextDouble() throws IOException { return Double.parseDouble(next()); }
+    static char nextChar() throws IOException { return next().charAt(0); }
+
+    static String nextLine() throws IOException {
+        if (st != null && st.hasMoreTokens()) {
+            StringBuilder rest = new StringBuilder(st.nextToken());
+            while (st.hasMoreTokens()) rest.append(' ').append(st.nextToken());
+            return rest.toString();
+        }
+        return br.readLine();
+    }
+
+    static int[] nextIntArray(int n) throws IOException {
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = nextInt();
+        return arr;
+    }
+
+    static long[] nextLongArray(int n) throws IOException {
+        long[] arr = new long[n];
+        for (int i = 0; i < n; i++) arr[i] = nextLong();
+        return arr;
+    }
+
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        int t = nextInt();
+        while (t-- > 0) 
+            solve();
+        
+        System.out.print(sb);
+        System.out.flush();
+    }
+
+    // Passes on CF
+    static void solve() throws IOException {
+        String s = next();
+        char[] chars = s.toCharArray();
+        
+        if (chars[0] != chars[chars.length - 1]) {
+            chars[0] = chars[chars.length - 1];
+        }
+        
+        sb.append(new String(chars)).append("\n");
+    }
+}
